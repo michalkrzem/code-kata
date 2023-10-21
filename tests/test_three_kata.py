a386b44f73503f1e6a158f888dcc21c5d6f657b1
mkrzeminski <mkrzeminski@imgw.pl>
2023-10-22 01:19:49 +0200

    Bad DNA tests and functionality added

--- tests/test_three_kata.py
+++ tests/test_three_kata.py
@@ -5,3 +5,10 @@ def test_three_kata_ok():
     dna = "GACCGCCGCC"
     result = three_kata.dna_to_rna(dna)
     assert result == "GACCGCCGCC"
+
+
+def test_three_kata_bad_dna():
+    dna = "GACCGCqecvbnCGCC"
+    result = three_kata.dna_to_rna(dna)
+
+    assert result == "Bad DNA"
