e16d3664374bcf05622fc8eebebef71e2ac0c663
mkrzeminski <mkrzeminski@imgw.pl>
2023-10-23 12:46:35 +0200

    Multiplier with decorator added

---
+++ kata/four_kata.py
@@ -0,0 +1,31 @@
+#  https://www.codewars.com/kata/55bf01e5a717a0d57e0000ec/train/python
+
+def count_calls(func):
+
+    def wrapper(*args, **kwargs):
+        if len(str(args[0])) == 1:
+            wrapper.counter = 0
+            return wrapper.counter
+        else:
+            wrapper.counter += 1
+            func(*args, **kwargs)
+            return wrapper.counter
+
+    wrapper.counter = 0
+
+    return wrapper
+
+
+@count_calls
+def persistence(n):
+    multiplier = 1
+    numbers = [int(number) for number in str(n)]
+    for number in numbers:
+        multiplier *= number
+
+    if len(str(multiplier)) > 1:
+        persistence(multiplier)
+
+
+persist = persistence(81110)
+print(persist)
